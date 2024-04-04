f52c8af84bfef3b87510a36e41d02212595d0053
nana <comfortappiah.ca@gmail.com>
2024-04-04 23:58:37 +0000

    done fetching data from advice api

--- src/componenes/Advice.jsx
+++ src/componenes/Advice.jsx
@@ -1,9 +1,49 @@
-import {useState, useEffect} from 'react'
+import { useState, useEffect } from "react";
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+import Form from "react-bootstrap/Form";
 
 const Advice = () => {
-  return (
-    <div>Advice</div>
-  )
-}
+	const [data, setData] = useState({});
 
-export default Advice
+  useEffect(()=>{
+    fetch("https://api.adviceslip.com/advice")
+			.then((data) => data.json())
+			.then((response) => setData(response.slip));
+console.log(data);
+  },[])
+
+	return (
+		<div className='container m-5 justify-content-center' >
+			<div className='row'>
+				<Form  className='d-flex'>
+					<Form.Control
+						type='search'
+						placeholder='Search'
+						className='me-2'
+						aria-label='Search'
+					/>
+					<Button variant='outline-success'>Search</Button>
+				</Form>
+
+      
+          	<div  className='col'>
+					<Card style={{ width: "18rem" }}>
+						<Card.Img variant='top' src='holder.js/100px180' />
+						<Card.Body>
+							<Card.Title>{data.id}</Card.Title>
+							<Card.Text>
+							{data.advice}
+							</Card.Text>
+							<Button variant='primary'>Go somewhere</Button>
+						</Card.Body>
+					</Card>
+				</div>
+         
+			
+			</div>
+		</div>
+	);
+};
+
+export default Advice;
