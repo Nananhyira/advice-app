148a4625334bdf5c44c34528bb09a2b38233042c
nana <comfortappiah.ca@gmail.com>
2024-04-06 01:42:33 +0000

    done fetching data

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
 
   return (
     <div>
-     <Advice/>
+     {/* <Advice/> */}
      <Recipe/>
        
     </div>
--- src/componenes/Recipe.jsx
+++ src/componenes/Recipe.jsx
@@ -1,18 +1,63 @@
-import {useState, useEffect} from 'react'
+import { useState, useEffect } from "react";
+import Card from "react-bootstrap/Card";
 
-const apiKey = import.meta.env.VITE_MY_API_KEY
+const apiKey = import.meta.env.VITE_MY_API_KEY;
+
 
 
 const Recipe = () => {
-  const [recipe , setRecipe]=useState({})
-   useEffect(() =>{
-    fetch(`https://api.spoonacular.com/recipes/random?number=100&apiKey=${apiKey}`).then((res) => res.json())
-			.then((result) => setRecipe(result))
-   },[])
-console.log(recipe)
-  return (
-    <div>Recipe</div>
-  )
-}
+	const [recipe, setRecipe] = useState({});
+  //  const fetchData = async () => {
+	// 		try {
+	// 			const data = await fetch(options.url, options.headers);
+	// 			 const res = await data.json();
+	// 			setRecipe(res);
+	// 		} catch (error) {
+	// 			console.log(error);
+	// 		}
+	// 	};
+	useEffect(() => {
+    // fetchData()
+		fetch(
+			`https://api.spoonacular.com/recipes/random?number=100&apiKey=${apiKey}`
+		)
+			.then((res) => res.json())
+			.then((result) => setRecipe(result));
+	}, []);
 
-export default Recipe
+
+	console.log(recipe);
+	return (
+		<div className='container m-5 j'>
+			<div className='row'>
+				<div className='col-3  justify-content-center'>
+					{/* {recipe.map((item, index) => {
+						return (
+							<Card
+								key={index}
+								className='justify-content-center'
+								style={{
+									width: "25rem",
+									height: "20rem",
+									backgroundColor: "red",
+								}}>
+								<Card.Body>
+									<Card.Title
+										className='text-center'
+										style={{ color: "white", fontSize: "20px" }}>
+										Advice Number:
+									</Card.Title>
+									<Card.Text
+										className='text-center'
+										style={{ color: "white", fontSize: "30px" }}></Card.Text>
+								</Card.Body>
+							</Card>
+						);
+					})} */}
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default Recipe;
