20ae5681b7d7b0b703000a0aaa3b780845b7f106
nana <comfortappiah.ca@gmail.com>
2024-04-10 00:45:55 +0000

    errors

--- src/componenes/Form.jsx
+++ src/componenes/Form.jsx
@@ -6,24 +6,44 @@ const Form = () => {
 	const [name, setName] = useState("");
 	const [email, setEmail] = useState("");
 
+const handleSubmit =() =>{
+  // e.preventDefault();
+  let user= {name:name, email:email}
+setUsers(user)
+console.log(users, user);
 
+}
 
 
 	return (
 		<div className='container m-5 j'>
 			<div className='row'>
 				<div className='col-md-12  justify-content-center'>
-			<h1>Register form</h1>
-			<form>
-				<input type='text' placeholder='name' />
-				<input type='text' placeholder='email' />
-				<Button variant='primary'>submit</Button>
-			</form>
-      </div>
-<div className='col-md-6'>
-
-</div>
-      </div>
+					<h1>Register form</h1>
+					<form onSubmit={handleSubmit()}>
+						<input
+							type='text'
+							placeholder='name'
+							value={name}
+							onChange={(e) => {
+								setName(e.target.value);
+							}}
+						/>
+						<input
+							type='text'
+							placeholder='email'
+							value={email}
+							onChange={(e) => {
+								setEmail(e.target.value);
+							}}
+						/>
+						<Button type='submit' variant='primary'>
+							submit
+						</Button>
+					</form>
+				</div>
+				<div className='col-md-6'></div>
+			</div>
 		</div>
 	);
 };
