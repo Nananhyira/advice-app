3ef24f9e26592be1c0e75dfdac0524d7f5a61aba
nana <comfortappiah.ca@gmail.com>
2024-04-17 00:00:18 +0000

    j

--- src/componenes/Search.jsx
+++ src/componenes/Search.jsx
@@ -1,37 +1,36 @@
-import { useState,useEffect } from 'react'
-import axios from "axios"
-
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 const Search = () => {
-  const [data, setData]=useState([])
-  const [v, setV]=useState(data)
+	const [data, setData] = useState([]);
+	const [v, setV] = useState(data);
 
-  	useEffect(()=>{
-		axios.get("https://jsonplaceholder.typicode.com/users").then((response)=>{setData(response.data)})
+	useEffect(() => {
+		axios.get("https://jsonplaceholder.typicode.com/users").then((response) => {
+			setData(response.data);
+			setV(response.data);
+		});
+	}, []);
+	const handleChange = (e) => {
+		const val = e.target.value;
+		setV(data.filter((i) => i.name.includes(val)));
+	};
 
-	},[])
-  const handleChange=(e)=>{
-    const val = e.target.value
-setV(data.filter(i=>i.name.includes(val)))
+	return (
+		<div>
+			<h1>Search</h1>
+			<input type='text' onChange={handleChange} placeholder='search' />
+			<h1>{v}</h1>
+			{v.map((item, index) => {
+				return (
+					<div key={index}>
+						<h1>{item.name}</h1>
+						<h3></h3>
+					</div>
+				);
+			})}
+		</div>
+	);
+};
 
-  }
-
-  return (
-    <div>
-      <h1>Search</h1>
-      <input type="text" onChange={handleChange} value={v}/>
-      <h1>{v}</h1>
-     {data.map((item, index)=>{
-      return(
-       <div key={index}>
-        <h1>
-         {item.name}
-        </h1>
-         <h3></h3>
-      </div>
-     )})}
-    </div>
-  )
-}
-
-export default Search
+export default Search;
