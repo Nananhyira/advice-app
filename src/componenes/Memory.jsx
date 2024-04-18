5dc89a3e2ce242aee0c727721aa7bc79e6fb6a0a
nana <comfortappiah.ca@gmail.com>
2024-04-18 23:12:52 +0000

    maping through a 3d array

--- src/componenes/Memory.jsx
+++ src/componenes/Memory.jsx
@@ -1,7 +1,23 @@
 import {useState} from 'react'
 
 const Memory = () => {
+  const [grid , setGrid]=useState([
+    [0,3,5,1], 
+    [1,2,2,4],
+    [4,3,5,0]
+  ])
   return (
+    {grid.map((row, rowIndex)=>{
+      return(
+        <div  key={rowIndex} className="row">
+        {row.map((col, colIndex)=>{
+          return(
+            <div></div>
+          )})}
+        </div>
+      )
+
+    })}
     <div>Memory</div>
   )
 }
