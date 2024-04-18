1a39633c81dd3bc08727e2e796a81a523b9a982a
nana <comfortappiah.ca@gmail.com>
2024-04-18 23:41:16 +0000

    styling

--- src/componenes/Memory.jsx
+++ src/componenes/Memory.jsx
@@ -1,22 +1,28 @@
-import {useState} from 'react'
+import { useState } from "react";
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
 
 const Memory = () => {
-  const[grid, setGrid] = useState(
-    [
-    [0,3,5,1,], 
-    [1,2,2,4,],
-    [4,3,5,0,],
-  ])
-  
-  return (
-		<div>
+	const [grid, setGrid] = useState([
+		[0, 3, 5, 1],
+		[1, 2, 2, 4],
+		[4, 3, 5, 0],
+	]);
+
+	return (
+		<div className="container">
 			{grid.map((row, rowIndex) => {
 				return (
 					<div key={rowIndex} className='row'>
 						{row.map((col, colIndex) => {
 							return (
-								<div key={colIndex} className='col'>
-									{col}
+								<div className='col-3' style={{ padding: "5px" }} key={colIndex} >
+									<Card style={{ width: "15rem", padding: "5px" }}>
+										<Card.Img variant='top' src='holder.js/100px180' />
+										<Card.Body>
+											<Card.Title>{col}</Card.Title>
+										</Card.Body>
+									</Card>
 								</div>
 							);
 						})}
@@ -26,6 +32,6 @@ const Memory = () => {
 			<div>Memory</div>
 		</div>
 	);
-}
+};
 
-export default Memory
+export default Memory;
