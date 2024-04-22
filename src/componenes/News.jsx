15033281e8266cec586f93189ba4e19826f646c3
nana <comfortappiah.ca@gmail.com>
2024-04-22 23:57:25 +0000

    done creating a rating component

--- src/componenes/News.jsx
+++ src/componenes/News.jsx
@@ -8,24 +8,35 @@ const News = () => {
 			<h1>Rating Customers</h1>
 			<i
 				className='bi bi-star-fill  self'
-				style={{ fontSize: "10rem" }}
+				style={{ color: rate > 0 ? "red" : "green", fontSize: "5rem" }}
 				onClick={() => {
-					setRate(5);
+					setRate(1);
 				}}></i>
-			<i className='bi bi-star-fill self' style={{ fontSize: "8rem" }} onClick={() => {
-					setRate(4);
-				}}></i>
-			<i className='bi bi-star-fill self' style={{ fontSize: "6rem" }} onClick={() => {
-					setRate(3);
-				}}></i>
-			<i className='bi bi-star-fill self' style={{ fontSize: "5rem" }}onClick={() => {
+			<i
+				className='bi bi-star-fill self'
+				style={{ color: rate > 0 ? "red" : "green", fontSize: "6rem" }}
+				onClick={() => {
 					setRate(2);
 				}}></i>
-			<i className='bi bi-star-half self' style={{ fontSize: "5rem" }} onClick={() => {
+			<i
+				className='bi bi-star-fill self'
+				style={{ color: rate > 0 ? "red" : "green", fontSize: "8rem" }}
+				onClick={() => {
+					setRate(3);
+				}}></i>
+			<i
+				className='bi bi-star-fill self'
+				style={{ color: rate > 0 ? "red" : "green", fontSize: "10rem" }}
+				onClick={() => {
+					setRate(4);
+				}}></i>
+			<i
+				className='bi bi-star-half self'
+				style={{ color: rate > 0 ? "red" : "green", fontSize: "12rem" }}
+				onClick={() => {
 					setRate(4.5);
 				}}></i>
-{rate>0&&<h1>your rating:{rate}</h1>}
-			
+			{rate > 0 && <h1>your rating:{rate}</h1>}
 		</div>
 	);
 };
